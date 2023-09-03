bc9208c9dfc67d803258830f8451745cae8d4062
Zlatoslav Desyatnikov <px.zlodes@gmail.com>
2023-09-03 19:23:36 +0400

    refactor(metric): Drop Metric interface in favour of abstract class

--- src/Metric/Gauge.php
+++ src/Metric/Gauge.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Zlodes\PrometheusClient\Metric;
 
-final class Gauge extends BaseMetric
+final class Gauge extends Metric
 {
     public function getPrometheusType(): string
     {
--- src/Metric/Metric.php
+++ src/Metric/Metric.php
@@ -4,30 +4,50 @@ declare(strict_types=1);
 
 namespace Zlodes\PrometheusClient\Metric;
 
-interface Metric
+use Webmozart\Assert\Assert;
+
+/**
+ * This class shouldn't be used directly anywhere.
+ */
+abstract class Metric
 {
     /**
-     * @return string Will be shown near metric output
+     * @param non-empty-string $name Metric name
+     * @param non-empty-string $help Metric help (will be shown near metric output)
      */
-    public function getPrometheusType(): string;
+    final public function __construct(
+        private readonly string $name,
+        private readonly string $help,
+    ) {
+        Assert::regex($name, '/^[a-zA-Z][a-zA-Z0-9_:]*$/', 'Metric name MUST be in snake case');
+    }
+
+    /**
+     * @return string Will be shown in metric output
+     */
+    abstract public function getPrometheusType(): string;
 
     /**
      * @return non-empty-string Metric name
      */
-    public function getName(): string;
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
     /**
      * @return non-empty-string Will be shown near metric output
      */
-    public function getHelp(): string;
+    public function getHelp(): string
+    {
+        return $this->help;
+    }
 
     /**
-     * @return array<non-empty-string, non-empty-string>
+     * @return list<string> Names of metrics that should be output beside this metric
      */
-    public function getInitialLabels(): array;
-
-    /**
-     * @return list<non-empty-string> List of dependent metrics, e.g. metrics with _count and _sum suffix for Histogram
-     */
-    public function getDependentMetrics(): array;
+    public function getDependentMetrics(): array
+    {
+        return [];
+    }
 }

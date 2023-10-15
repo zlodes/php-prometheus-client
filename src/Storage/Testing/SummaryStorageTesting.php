3461da95e8ae09436949ac6a7dc886534ae1673b
Zlatoslav Desyatnikov <px.zlodes@gmail.com>
2023-10-15 21:05:11 +0400

    test(summary): Improve SummaryStorageTesting
    BREAKING CHANGE: To mark major release

--- src/Storage/Testing/SummaryStorageTesting.php
+++ src/Storage/Testing/SummaryStorageTesting.php
@@ -10,6 +10,7 @@ use Zlodes\PrometheusClient\Storage\DTO\MetricNameWithLabels;
 use Zlodes\PrometheusClient\Storage\DTO\SummaryMetricValue;
 
 // phpcs:ignore
+use function PHPUnit\Framework\assertContains;
 use function PHPUnit\Framework\assertCount;
 use function PHPUnit\Framework\assertEmpty;
 use function PHPUnit\Framework\assertEquals;
@@ -65,20 +66,28 @@ trait SummaryStorageTesting
         $fetched = [...$storage->fetchSummaries()];
         assertCount(3, $fetched);
 
-        // memory_usage foo
-        assertSame('memory_usage', $fetched[0]->metricNameWithLabels->metricName);
-        assertSame(['foo' => 'bar'], $fetched[0]->metricNameWithLabels->labels);
-        assertEquals([300, 500], $fetched[0]->elements);
+        // sort fetched values to have predictable order
+        usort($fetched, static function  (SummaryMetricValue $a, SummaryMetricValue $b) {
+            $keyA = $a->metricNameWithLabels->metricName . ':' . serialize($a->metricNameWithLabels->labels);
+            $keyB = $b->metricNameWithLabels->metricName . ':' . serialize($b->metricNameWithLabels->labels);
+
+            return $keyA <=> $keyB;
+        });
+
+        // cpu_usage
+        assertSame('cpu_usage', $fetched[0]->metricNameWithLabels->metricName);
+        assertSame([], $fetched[0]->metricNameWithLabels->labels);
+        assertEquals([0.345, 1.25], $fetched[0]->elements);
 
         // memory_usage bar
         assertSame('memory_usage', $fetched[1]->metricNameWithLabels->metricName);
         assertSame(['bar' => 'baz'], $fetched[1]->metricNameWithLabels->labels);
         assertEquals([100], $fetched[1]->elements);
 
-        // cpu_usage
-        assertSame('cpu_usage', $fetched[2]->metricNameWithLabels->metricName);
-        assertSame([], $fetched[2]->metricNameWithLabels->labels);
-        assertEquals([0.345, 1.25], $fetched[2]->elements);
+        // memory_usage foo
+        assertSame('memory_usage', $fetched[2]->metricNameWithLabels->metricName);
+        assertSame(['foo' => 'bar'], $fetched[2]->metricNameWithLabels->labels);
+        assertEquals([300, 500], $fetched[2]->elements);
 
         $storage->clearSummaries();
         assertEmpty([...$storage->fetchSummaries()]);
